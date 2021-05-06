29bf7bda852d184d12f28b1381625d5b942655ce
Karan Raina <karanraina1996@gmail.com>
2021-05-06 21:33:27 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,83 @@
+/* eslint-disable react-native/no-inline-styles */
+import * as React from 'react';
+import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
+import {NavigationContainer} from '@react-navigation/native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import {Container, Root} from 'native-base';
+
+import HomeScreen from './screens/Homescreen';
+import NotificationScreen from './screens/NotificationScreen';
+import AboutScreen from './screens/AboutScreen';
+import LinksScreen from './screens/LinksScreen';
+
+import {PRIMARY_LIGHT_COLOR, PRIMARY_TEXT_COLOR} from '../config/colors';
+
+const Tab = createMaterialBottomTabNavigator();
+
+export default function App() {
+  return (
+    <Root>
+      <Container>
+        <NavigationContainer>
+          <Tab.Navigator
+            initialRouteName="Home"
+            activeColor={PRIMARY_TEXT_COLOR}
+            inactiveColor="#9e9e9e"
+            shifting={false}
+            labelStyle={{fontSize: 10}}
+            barStyle={{backgroundColor: PRIMARY_LIGHT_COLOR}}>
+            <Tab.Screen
+              name="Home"
+              component={HomeScreen}
+              options={{
+                tabBarLabel: 'Home',
+                tabBarIcon: ({color}) => (
+                  <MaterialCommunityIcons name="home" color={color} size={26} />
+                ),
+              }}
+            />
+            <Tab.Screen
+              name="Gallery"
+              component={AboutScreen}
+              options={{
+                tabBarLabel: 'About us',
+                tabBarIcon: ({color}) => (
+                  <MaterialCommunityIcons
+                    name="information-outline"
+                    color={color}
+                    size={26}
+                  />
+                ),
+              }}
+            />
+            {/* <Tab.Screen
+              name="Sharda Translator"
+              component={NotificationScreen}
+              options={{
+                tabBarLabel: 'Notifications',
+                tabBarIcon: ({color}) => (
+                  <MaterialCommunityIcons
+                    name="bell-alert"
+                    color={color}
+                    size={26}
+                  />
+                ),
+              }}
+            />
+            <Tab.Screen
+              name="Links"
+              component={LinksScreen}
+              options={{
+                tabBarLabel: 'More',
+                tabBarIcon: ({color}) => (
+                  <MaterialCommunityIcons name="link" color={color} size={26} />
+                ),
+              }}
+            /> */}
+          </Tab.Navigator>
+        </NavigationContainer>
+      </Container>
+    </Root>
+  );
+}
---
+++ src/screens/LearnSharda.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import {BackHandler, Platform} from 'react-native';
+
+import {WebView} from 'react-native-webview';
+import {HeaderBackButton} from '@react-navigation/stack';
+
+export default ({navigation}) => {
+  const [webViewref, setRef] = React.useState();
+  const [canGoBack, setGoBack] = React.useState();
+
+  const onAndroidBackPress = React.useCallback(() => {
+    if (canGoBack && webViewref) {
+      webViewref.goBack();
+      return true;
+    }
+    navigation.navigate('Home');
+    return false;
+  });
+  React.useEffect(() => {
+    if (Platform.OS === 'android') {
+      BackHandler.addEventListener('hardwareBackPress', onAndroidBackPress);
+    }
+    // Specify how to clean up after this effect:
+    return function cleanup() {
+      if (Platform.OS === 'android') {
+        BackHandler.removeEventListener('hardwareBackPress');
+      }
+    };
+  });
+
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      headerLeft: props => (
+        <HeaderBackButton {...props} onPress={onAndroidBackPress} />
+      ),
+    });
+  }, [navigation, onAndroidBackPress]);
+
+  return (
+    <WebView
+      source={{uri: 'https://karankraina.github.io/learning/main.html'}}
+      ref={webView => {
+        setRef(webView);
+      }}
+      onNavigationStateChange={navState => {
+        setGoBack(navState.canGoBack);
+      }}
+    />
+  );
+};
